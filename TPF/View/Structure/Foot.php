93f0b1f69ee65f64e9dcb4b7bb7b5fad3c480dd8
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-11-16 00:38:49 -0300

    Estructura y styles modificados. Aclaraciones de diseño en excalidraw.

--- TPF/View/Structure/Foot.php
+++ TPF/View/Structure/Foot.php
@@ -4,12 +4,13 @@
         <footer class="contenedor-inactivo-steam py-3 mt-auto">    
             <div class="mx-4 d-flex flex-wrap justify-content-between align-items-center">
                 <div class="d-flex">
-                    <span> &copy; Grupo 4 - Programación Web Dinámica año 2024 </span>
+                    <span> &copy; Benitez, Franco Fabian [FAI-3169]; PWD 2024 </span>
                 </div>
             </div>
         </footer>
 
-        <?php include (INCLUDE_PATH."/Scripts.php"); ?> <!-- Rutas de scripts a usar (ej. validaciones) -->
+        <!-- Rutas de scripts a usar (ej. validaciones) -->
+        <script src="<?php echo BASE_URL; ?>/View/Assets/js/validacionesTP04.js"></script>
 
     </body>
 </html>
--- TPF/View/Structure/HeadSafe.php
+++ TPF/View/Structure/HeadSafe.php
@@ -25,8 +25,28 @@
         
     </head>
 
+    <?php
+        //Verificacion de sesion valida (para mostrar pagina segura)
+        if(!(new Session())->validar()) {
+            $mensaje ="Error, vuelva a intentarlo";
+            echo($mensaje);
+            //header("Location: ".ROOT_PATH."/index.php");
+            exit();
+        }
+    ?>
+
     <body class="d-flex flex-column min-vh-100"> <!-- Comienza body -->
         <header>
+
+        <nav class="navbar navbar-expand-sm contenedor-inactivo-steam">
+    <div class="mx-2 my-1">
+        <a class="btn btn-primary btn-steam3 mx-2" href="<?php echo BASE_URL; ?>/index.php">Inicio</a> <!-- Index button -->
+    </div>
+    <div class="mx-3">
+        <a class="btn btn-primary btn-steam my-1" href="<?php echo BASE_URL ;?>/View/Ejercicios/Tienda/Tienda.php"> Tienda </a>
+    </div>
+</nav>
+            
             <!-- Barra de navegación -->
             <nav class="navbar navbar-expand-sm contenedor-inactivo-steam justify-content-between">
             <div class="mx-2 my-1">
