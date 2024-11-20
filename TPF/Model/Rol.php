0d117ad20a33f74b401d32977b2f50c42b96c148
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-11-20 02:32:00 -0300

    Rol listo

--- TPF/Model/Rol.php
+++ TPF/Model/Rol.php
@@ -2,65 +2,60 @@
 
 include_once 'BaseDatos.php';
 
-class Rol
-{
+class Rol {
     private $idrol;
-    private $roDescripcion;
+    private $rodescripcion;
     private $mensajeOperacion;
 
-    public function __construct($roDescripcion = null)
-    {
-        $this->roDescripcion = $roDescripcion;
+    public function __construct($idrol = null, $rodescripcion = null) {
+        $this->idrol = $idrol;
+        $this->rodescripcion = $rodescripcion;
     }
+
     // getters 
-    public function getIdrol()
-    {
+    public function getIdrol() {
         return $this->idrol;
     }
-    public function getRoDescripcion()
-    {
-        return $this->roDescripcion;
+    
+    public function getRodescripcion() {
+        return $this->rodescripcion;
     }
-    public function getMensajeOperacion()
-    {
+
+    public function getMensajeOperacion() {
         return $this->mensajeOperacion;
     }
+
     // setters
-    public function setIdrol($idrol)
-    {
+    public function setIdrol($idrol) {
         $this->idrol = $idrol;
     }
-    public function setRoDescripcion($rolDesc)
-    {
-        $this->roDescripcion = $rolDesc;
+    public function setRodescripcion($rodescripcion) {
+        $this->rodescripcion = $rodescripcion;
     }
-    public function setMensajeOperacion($mensajeOperacion)
-    {
+
+    public function setMensajeOperacion($mensajeOperacion) {
         $this->mensajeOperacion = $mensajeOperacion;
     }
+    
     // metodos CRUD
-
-    public function cargarDatos($idrol, $rolDescripcion = null)
-    {
-        $this->setRoDescripcion($rolDescripcion);
+    public function cargarDatos($idrol = null, $rodescripcion = null) {
+        $this->setRoDescripcion($rodescripcion);
         $this->setIdrol($idrol);
     }
-
-
+    
     /**
-     * Buscar datos de un usuario por su id
-     * @param int $id
+     * Buscar datos de un rol por su id
+     * @param int $idrol
      * @return boolean
      */
-    public function buscarDatos($id)
-    {
+    public function buscarDatos($idrol) {
         $bd = new BaseDatos();
         $resultado = false;
         if ($bd->Iniciar()) {
-            $consulta = "SELECT * FROM rol WHERE idrol = $id";
+            $consulta = "SELECT * FROM rol WHERE idrol = $idrol";
             if ($bd->Ejecutar($consulta)) {
                 if ($row = $bd->Registro()) {
-                    $this->cargarDatos($row['idrol'], $row['rodescripcion']);
+                    $this->cargarDatos($idrol, $row['rodescripcion']);
                     $resultado = true;
                 }
             }
@@ -68,70 +63,45 @@ class Rol
         return $resultado;
     }
 
-
-    public function cargar()
-    {
-        $resp = false;
-        $base = new BaseDatos();
-        $sql = "SELECT * FROM rol WHERE idrol = " . $this->getIdrol();
-
-        if ($base->Iniciar()) {
-            $res = $base->Ejecutar($sql);
-            if ($res > -1) {
-                if ($res > 0) {
-                    $row = $base->Registro();
-                    $this->cargarDatos(
-                        $row['idrol'],
-                        $row['rodescripcion'],
-                    );
-                }
-            }
-        } else {
-            $this->setMensajeOperacion("Tabla->listar: " . $base->getError());
-        }
-
-        return $resp;
-    }
-
     /**
      * Retorna una coleccion de roles donde se cumpla $condicion
      * @param $condicion // WHERE de sql
      * @return array // roles que cumplieron la condicion
      */
-    public function listar($condicion = "")
-    {
+    public function listar($condicion = "") {
         $coleccion = [];
-        $bd = new BaseDatos();
-        if ($bd->Iniciar()) {
+        $base = new BaseDatos();
+        if ($base->Iniciar()) {
             $consulta = "SELECT * FROM rol";
             if ($condicion != "") {
-                $consulta = $consulta . ' WHERE ' . $condicion;
+                $consulta = $consulta.' WHERE '.$condicion;
             }
-            $consulta .= " order by idrol";
-            if ($bd->Ejecutar($consulta)) {
-                while ($row = $bd->Registro()) {
-                    $rol = new Rol();
-                    $rol->cargarDatos($row['idrol'], $row['rodescripcion']);
-                    array_push($coleccion, $rol);
+            $consulta .= " ORDER BY idrol ";
+            if ($base->Ejecutar($consulta)) {
+                while ($row = $base->Registro()) {
+                    $obj = new Rol();
+                    $obj->cargarDatos($row['idrol'], $row['rodescripcion']);
+                    array_push($coleccion, $obj);
                 }
             } else {
-                $this->setMensajeOperacion($bd->getError());
+                $this->setmensajeOperacion($base->getError());
             }
         } else {
-            $this->setMensajeOperacion($bd->getError());
+            $this->setmensajeOperacion($base->getError());
         }
         return $coleccion;
     }
+
     /**
      * Insertar los datos de un rol a la bd
      * @return boolean
      */
-    public function insertar()
-    {
+    public function insertar() {
         $resultado = false;
         $bd = new BaseDatos();
         if ($bd->Iniciar()) {
-            $consulta = "INSERT INTO rol(rodescripcion) VALUES ('" . $this->getRoDescripcion() . "')";
+            $consulta = "INSERT INTO rol(rodescripcion) VALUES 
+            ('".$this->getRodescripcion()."')";
             if ($bd->Ejecutar($consulta)) {
                 $resultado = true;
             } else {
@@ -142,17 +112,18 @@ class Rol
         }
         return $resultado;
     }
-
+    
     /**
-     * Modificar los datos de un rol en la bd
+     * Modificar los datos de un rol con los que tiene el objeto actual 
      * @return boolean
      */
-    public function modificar()
-    {
+    public function modificar() {
         $resultado = false;
         $bd = new BaseDatos();
         if ($bd->Iniciar()) {
-            $consulta = "UPDATE rol SET rodescripcion = '" . $this->getRoDescripcion() . "' WHERE idrol = " . $this->getIdrol();
+            $consulta = "UPDATE rol 
+            SET rodescripcion = ".$this->getRodescripcion()."
+            WHERE idrol = ".$this->getIdrol();
             if ($bd->Ejecutar($consulta)) {
                 $resultado = true;
             } else {
@@ -168,12 +139,11 @@ class Rol
      * Eliminar un rol de la bd
      * @return boolean
      */
-    public function eliminar()
-    {
-        $bd = new BaseDatos();
+    public function eliminar() {
         $resultado = false;
+        $bd = new BaseDatos();
         if ($bd->Iniciar()) {
-            $consulta = "DELETE FROM rol WHERE idrol = " . $this->getIdrol();
+            $consulta = "DELETE FROM rol WHERE idrol = ".$this->getIdrol();
             if ($bd->Ejecutar($consulta)) {
                 $resultado = true;
             } else {
@@ -187,10 +157,11 @@ class Rol
 
     /**
      * Retorna un string con los datos del rol
-     * @return string
+     * @return String
      */
-    public function __tostring()
-    {
-        return ("Idrol: " . $this->getIdrol() . "\nDescripcion: " . $this->getRoDescripcion());
+    public function __toString() {
+        return ("idrol: ".$this->getIdrol()."\n".
+            "rodescripcion: ".$this->getRodescripcion());
     }
 }
+?>
