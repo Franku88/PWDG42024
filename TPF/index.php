93c5aff78344d1c038c53a437d67117d3b8e0a6f
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-11-20 19:10:24 -0300

    fix styles

--- TPF/index.php
+++ TPF/index.php
@@ -4,15 +4,15 @@ include STRUCTURE_PATH . '/Head.php';
 ?>
 
 <!-- <body> ...-->
-    <div class="text-center p-2 contenedor-inactivo-steam mx-auto rounded-4" style="margin-top: 150px;">
+    <div class="text-center p-2 bg-steam-lightgreen bdr-steam-nofocus mx-auto rounded-4" style="margin-top: 150px;">
         <h2 class="m-4">Programación Web Dinámica - Grupo 4</h2>
-        <div class="contenedor-activo-steam rounded-4 m-4">
+        <div class="bg-steam-darkgreen bdr-steam-focus rounded-4 m-4">
             <h4 class="m-4">[FAI-3169] Benitez, Franco Fabian<br><a href='mailto:fabian.benitez@est.fi.uncoma.edu.ar'>fabian.benitez@est.fi.uncoma.edu.ar</a></h4>
         </div>
-        <div class="contenedor-activo-steam rounded-4 m-4">
+        <div class="bg-steam-darkgreen bdr-steam-focus rounded-4 m-4">
             <h4 class="m-4">[FAI-4594] Pesce, Matías Nicolás<br><a href='mailto:matias.pesce@est.fi.uncoma.edu.ar'>matias.pesce@est.fi.uncoma.edu.ar</a></h4>
         </div>
-        <div class="contenedor-activo-steam rounded-4 m-4">
+        <div class="bg-steam-darkgreen bdr-steam-focus rounded-4 m-4">
             <h4 class="m-4">[FAI-3220] Reyes Castelló, José Vicente<br><a href='mailto:jose.reyes@est.fi.uncoma.edu.ar'>jose.reyes@est.fi.uncoma.edu.ar</a></h4>
         </div>
     </div>
