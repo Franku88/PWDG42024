870fe6471d3b17a013f973e05f195cabb03e7414
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-11-20 01:02:37 -0300

    listo

--- TPF/Controller/ABMCompraEstadoTipo.php
+++ TPF/Controller/ABMCompraEstadoTipo.php
@@ -1,89 +1,121 @@
 <?php
 
-include_once "/Applications/XAMPP/xamppfiles/htdocs/PWDG42024/TPF/Model/CompraEstadoTipo.php";
+// include_once "/Applications/XAMPP/xamppfiles/htdocs/PWDG42024/TPF/Model/CompraEstadoTipo.php"; Lo carga el autoloader.php
 
-class ABMCompraEstadoTipo
-
-{
-    private function cargarObjeto($param)
-    {
+class ABMCompraEstadoTipo {
+    
+    /**
+     * Espera como parametro un arreglo asociativo donde las claves coinciden 
+     * con los nombres de las variables instancias del objeto
+     * @param array $param
+     * @return CompraEstadoTipo
+     */
+    private function cargarObjeto($param) {
         $obj = null;
-        if (
-            array_key_exists('idcompraestadotipo', $param) and array_key_exists('cetdescripcion', $param)
-            and array_key_exists('cetdetalle', $param)
-        ) {
+        if (array_key_exists('cetdescripcion', $param) AND array_key_exists('cetdetalle', $param)) {
+            // Solo asignamos 'idcompraestadotipo' si está definido y es distinto de null
+            $idcompraestadotipo = array_key_exists('idcompraestadotipo', $param) ? $param['idcompraestadotipo'] : null;
             $obj = new CompraEstadoTipo();
-            $obj->setear($param['idcompraestadotipo'], $param['cetdescripcion'], $param['cetdetalle']);
+            $obj->cargarDatos($idcompraestadotipo, $param['cetdescripcion'], $param['cetdetalle']);
         }
         return $obj;
     }
 
-    private function cargarObjetoConClave($param)
-    {
+    /**
+     * Espera como parametro un arreglo asociativo donde las claves coinciden 
+     * con los nombres de las variables instancias del objeto que son claves
+     * @param array $param
+     * @return CompraEstadoTipo
+     */
+    private function cargarObjetoConClave($param) {
         $obj = null;
+        if ($this->seteadosCamposClaves($param)) {
+            $obj = new CompraEstadoTipo();
+            $obj->cargarDatos($param['idcompraestadotipo']);
+        }
+        return $obj;
+    }
 
+    /**
+     * Corrobora que dentro del arreglo asociativo estan seteados los campos claves
+     * @param array $param
+     * @return boolean
+     */
+    private function seteadosCamposClaves($param) {
+        $resp = false;
         if (isset($param['idcompraestadotipo'])) {
-            $obj = new CompraEstadoTipo();
-            $obj->setear($param['idcompraestadotipo'], null, null);
-        }
-        return $obj;
-    }
-
-    private function seteadosCamposClaves($param)
-    {
-        $resp = false;
-        if (isset($param['idcompraestadotipo']))
-            $resp = true;
-        return $resp;
-    }
-
-    public function alta($param)
-    {
-        $resp = false;
-        $setear = $this->cargarObjeto( $param); 
-        if ($setear != null and $setear->insertar()) {
             $resp = true;
         }
         return $resp;
     }
 
-    public function baja($param)
-    {
+    /**
+     * Inserta un compraestadotipo a la BD con atributos del arreglo ingresado
+     * @param array $param
+     * @return boolean
+     */
+    public function alta($param) {
+        $resp = false;
+        $obj = $this->cargarObjeto($param);
+        if ($obj != null && $obj->insertar()) {
+            $resp = true;
+        }
+        return $resp;
+    }
+
+    /**
+     * Elimina un compraestadotipo de la BD
+     * @param array $param
+     * @return boolean
+     */
+    public function baja($param) {
         $resp = false;
         if ($this->seteadosCamposClaves($param)) {
-            $elObjtCompraEstadoTipo = $this->cargarObjetoConClave($param);
-            if ($elObjtCompraEstadoTipo != null and $elObjtCompraEstadoTipo->eliminar()) {
+            $obj = $this->cargarObjetoConClave($param);
+            if ($obj != null && $obj->eliminar()) {
                 $resp = true;
             }
         }
         return $resp;
     }
 
-    public function modificacion($param)
-    {
-        //echo "Estoy en modificacion";
+    /**
+     * Modifica un compraestadotipo de la BD
+     * @param array $param
+     * @return boolean
+     */
+    public function modificacion($param) {
         $resp = false;
         if ($this->seteadosCamposClaves($param)) {
-            $elObjtCompraEstadoTipo = $this->cargarObjeto($param);
-            if ($elObjtCompraEstadoTipo != null and $elObjtCompraEstadoTipo->modificar()) {
+            $obj = $this->cargarObjeto($param);
+            if ($obj != null && $obj->modificar()) {
                 $resp = true;
             }
         }
         return $resp;
     }
 
-    public function buscar($param)
-    {
+    /**
+     * Busca un compraestadotipo en la BD
+     * Si $param es vacío, trae todos los compraestadotipo
+     * @param array $param
+     * @return array
+     */
+    public function buscar ($param = null) {
         $where = " true ";
-        if ($param <> NULL) {
-            if (isset($param['idcompraestadotipo']))
-                $where .= " and idcompraestadotipo =" . $param['idcompraestadotipo'];
-            if (isset($param['cetdescripcion']))
-                $where .= " and cetdescripcion =" . $param['cetdescripcion'];
-            if (isset($param['cetdetalle']))
-                $where .= " and cetdetalle ='" . $param['cetdetalle'] . "'";
+        if ($param != null) {
+            if (isset($param['idcompraestadotipo'])) {
+                $where = "AND idcompraestadotipo = ".$param['idcompraestadotipo'];
+            }
+            if (isset($param['cetdescripcion'])) {
+                $where = "AND cetdescripcion = '".$param['cetdescripcion']."'";
+            }
+            if (isset($param['cetdetalle'])) {
+                $where = "AND cetdetalle = '".$param['cetdetalle']."'";
+            }
         }
-        $arreglo = compraestadotipo::listar($where);
+        $arreglo = (new CompraEstadoTipo())->listar($where);
         return $arreglo;
     }
 }
+?>
