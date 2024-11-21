448f4e50d31bf8cea46e015a1b92920b809c9a37
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-11-20 22:46:16 -0300

    wip: falta agregar botones para el carro (ademas de adaptar header segun usuarioRol)

--- TPF/View/Ejercicios/Producto/Producto.php
+++ TPF/View/Ejercicios/Producto/Producto.php
@@ -1,5 +1,6 @@
 <?php
 include_once '../../../configuracion.php';
+include STRUCTURE_PATH . '/Head.php';
 
 $data = Funciones::data_submitted();
 
@@ -8,105 +9,69 @@ if (!empty($data)) {
     $resultado = null;
     if (!empty($arrProductos)) {
         $producto = $arrProductos[0];
-        $imagenesDiv = "";
-        $dirImagenes = "/View/Assets/Media/Product/".$producto->getIdproducto();
-        $dirImagenesDiv = $dirImagenes . '/Preview/*.png';
-        // glob escanea la dirección y devuelve un array con las imágenes en el directorio (no incluye subcarpetas)
-        $scanDiv = glob($dirImagenesDiv);
+        $dirImagenes = "../../Media/Product/".$producto->getIdproducto();
 
+        // glob escanea la dirección y devuelve un array con las imágenes en el directorio (no incluye subcarpetas)
+        $scanHeader = glob($dirImagenes.'/*.png');
+        if (!empty($scanHeader)) {
+            $header = "<img class='' src='".$scanHeader[0]."' alt='portada'>";
+        } else {
+            $header = '<p> Sin Portada </p>';
+        }
+        
+        $scanDiv = glob($dirImagenes."/Preview/*");
         if (!empty($scanDiv)) {
-            for ($i = 0; $i < count($scanDiv); $i++) {
-                $imagenesDiv = '<img src="' . $dirImagenes . '/Preview/img' . $i . '.png alt="imágen div ' . $i . '">';
+            $previewDiv = "";
+            foreach($scanDiv as $cadaRuta) {
+                $previewDiv .= "<img class='m-1' src='".$cadaRuta."'>";
             }
         } else {
-            $imagenesDiv = '<img src="' . $dirImagenes . '/Preview/default.png" alt="default">';
-        }
-
-        $dirImagenesPortada = $dirImagenes . '/*.png';
-        $scanPortada = glob($dirImagenesPortada);
-
-        if (!empty($scanPortada)) {
-            $imagenPortada = '<img src="' . $dirImagenes . '/icon.png" alt="portada">';
-        } else {
-            $imagenPortada = '<img src="' . $dirImagenes . '/default.png" alt="default">';
+            $previewDiv = '<p> Sin imagenes de muestra </p>'; //No hay imagenes de muestra
         }
 
         // Código HTML
-        $resultado =
+        $resultado = "
+        <div class='row h-100 m-5'>
+                <div class='col-6 bg-steam-lightgreen bdr-steam-nofocus align-content-center overflow-x-scroll'>
 
-            "<main class='borde-inactivo-steam'>
-        <div id=''>
-            <div class=''>
-                <div class=''>
-                    <div class='producto__resumen borde-activo-steam'>
-                        <div class='producto__data borde-inactivo-steam'>
-                            <div class='producto__portada__borde'>
-                                <div class='producto__portada__marco'>
-                                    <img id='producto__portada'>
-                                    <!-- PORTADA DEL PRODUCTO -->
-                                    " . $imagenPortada . "
-                                </div>
-                            </div>
-                            <div class='producto__descripcion'>
-                                <div>
-                                    <!--- TEXTO DESCRIPTIVO DEL PRODUCTO --->
-                                    <p id='producto__descripcion'> ".$producto->getProdetalle()." </p>
-                                </div>
-                            </div>
-                            <div id='producto__imagenes' class='borde-activo-steam'>
-                                " . $imagenesDiv . "
+                    <div class='row d-flex flex-column align-items-center justify-content-between p-2'>
+                        <div class='d-flex align-items-center justify-content-center overflow-x-scroll p-2'>
+                            <!-- PORTADA DEL PRODUCTO -->
+                            <div class='bg-steam-lightgreen bdr-steam-nofocus p-1'>
+                                " . $header . "
                             </div>
                         </div>
-                    </div>
-                    <div id='producto__video borde-activo-steam'>
-                        <!-- TRAILER/VIDEO/IMAGEN AL RESPECTO SI FUERA EL CASO -->
-                        <iframe width='800' height='600' class='borde-inactivo-steam' src='https://www.youtube.com/embed/CHm2d3wf8EU' title='Cruelty Squad Trailer' frameborder='0' allowfullscreen>
-                        </iframe>
-                    </div>
-                </div>
-            </div>
-            <div class='producto__bottom'>
-                <div class='producto__precios'>
-                    <p id='producto__precio__pesos'></p>
-                    <p id='producto__precio__dolares'></p>
-                </div>
-                <div class='producto__botones'>
-                    <button class='boton__agregar' onclick='agregarAlCarroTitulo('Cruelty Squad', null)'>Agregar al carro</button>
-                    <button class='boton__carro' onclick='redirigir('/pages/carro.html')>Ir al carro</button>
-                </div>
-            </div>
-        </div>
-        <div>
 
+                        <div class='text-center'>
+                            <!--- TEXTO DESCRIPTIVO DEL PRODUCTO --->
+                            <p> ".$producto->getProdetalle()." </p>
+                        </div>
+                        
+                    </div>
+                    
+                    <div class='bg-steam-darkgreen bdr-steam-focus d-flex overflow-x-scroll h-25 w-75 m-auto'>
+                        ".$previewDiv."
+                    </div>
+                    
+                </div>
+
+                <div class='col-6 bg-steam-lightgreen bdr-steam-nofocus d-flex overflow-x-scroll'>
+                    <!-- TRAILER/VIDEO/IMAGEN AL RESPECTO SI FUERA EL CASO -->
+                    <div class='m-auto'>
+                        <iframe width='800' height='600' class='bg-steam-lightgreen bdr-steam-nofocus' src='https://www.youtube.com/embed/edYCtaNueQY?si=WbFaQlTLEAdBxTlt' frameborder='0' allowfullscreen></iframe>
+                    </div>
+                </div>                
         </div>
-    </main> ";
+           ";
+
     } else {
         $resultado = '<h2> PRODUCTO NO ENCONTRADO </h2>';
     }
 } else {
     $resultado = '<h2> NO SE HA SELECCIONADO UN PRODUCTO </h2>';
 }
-
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<?php echo $resultado; ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-
-    <H1> PRODUCTO GENERICO </H1>
-
-    <?php
-    echo $resultado;
-    ?>
-
-
-</body>
-
-</html>
+<?php include STRUCTURE_PATH . '/Foot.php'; ?>
