bdc5c275cc64fb005ee5db9836f7738a663e1edb
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-11-21 15:06:39 -0300

    fix: margin de contenedor principal

--- TPF/index.php
+++ TPF/index.php
@@ -4,7 +4,7 @@ include STRUCTURE_PATH . '/Head.php';
 ?>
 
 <!-- <body> ...-->
-    <div class="text-center p-2 bg-steam-lightgreen bdr-steam-nofocus mx-auto rounded-4" style="margin-top: 150px;">
+    <div class="text-center p-2 bg-steam-lightgreen bdr-steam-nofocus mx-auto my-auto rounded-4">
         <h2 class="m-4">Programación Web Dinámica - Grupo 4</h2>
         <div class="bg-steam-darkgreen bdr-steam-focus rounded-4 m-4">
             <h4 class="m-4">[FAI-3169] Benitez, Franco Fabian<br><a href='mailto:fabian.benitez@est.fi.uncoma.edu.ar'>fabian.benitez@est.fi.uncoma.edu.ar</a></h4>
