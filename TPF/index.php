94b35dcb04835547c80db9713f2ad99032cefd47
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-11-22 06:30:53 -0300

    insersión de logo al index

--- TPF/index.php
+++ TPF/index.php
@@ -4,7 +4,14 @@ include STRUCTURE_PATH . '/Head.php';
 ?>
 
 <!-- <body> ...-->
+    
     <div class="text-center p-2 bg-steam-lightgreen bdr-steam-nofocus mx-auto my-auto rounded-4">
+        <div class="mx-auto d-flex flex-column justify-content-center">
+            <div class="text-center my-1">
+                <img src="View/Media/Site/logo.png" height="100" width="100"> </img>
+            </div>
+            <h1 class="my-1">Trabajo Práctico Final</h1>
+        </div>
         <h2 class="m-4">Programación Web Dinámica - Grupo 4</h2>
         <div class="bg-steam-darkgreen bdr-steam-focus rounded-4 m-4">
             <h4 class="m-4">[FAI-3169] Benitez, Franco Fabian<br><a href='mailto:fabian.benitez@est.fi.uncoma.edu.ar'>fabian.benitez@est.fi.uncoma.edu.ar</a></h4>
