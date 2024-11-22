2cae78f52ffeb839dfd9adb9141d07a4ca68bb18
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-11-22 03:51:01 -0300

    prototipo, funciona con nueva bd (idvideoyt), falta implementarla con ajax

--- TPF/View/Ejercicios/Producto/Producto.php
+++ TPF/View/Ejercicios/Producto/Producto.php
@@ -14,7 +14,7 @@ if (!empty($data)) {
         // glob escanea la dirección y devuelve un array con las imágenes en el directorio (no incluye subcarpetas)
         $scanHeader = glob($dirImagenes.'/*.png');
         if (!empty($scanHeader)) {
-            $header = "<img class='' src='".$scanHeader[0]."' alt='portada'>";
+            $header = "<img class='' src='".$scanHeader[0]."' height='250' alt='portada'>";
         } else {
             $header = '<p> Sin Portada </p>';
         }
@@ -26,30 +26,35 @@ if (!empty($data)) {
                 $previewDiv .= "<img class='m-1' src='".$cadaRuta."'>";
             }
         } else {
-            $previewDiv = '<p> Sin imagenes de muestra </p>'; //No hay imagenes de muestra
+            $previewDiv = '<p class="m-auto"> Sin imagenes de muestra </p>'; //No hay imagenes de muestra
         }
 
         // Código HTML
         $resultado = "
         <div class='row h-100 m-5'>
-                <div class='col-6 bg-steam-lightgreen bdr-steam-nofocus align-content-center overflow-x-scroll'>
+                <div class='h-100 col-6 bg-steam-lightgreen bdr-steam-nofocus align-content-center overflow-x-scroll'>
 
                     <div class='row d-flex flex-column align-items-center justify-content-between p-2'>
                         <div class='d-flex align-items-center justify-content-center overflow-x-scroll p-2'>
                             <!-- PORTADA DEL PRODUCTO -->
-                            <div class='bg-steam-lightgreen bdr-steam-nofocus p-1'>
-                                " . $header . "
+                            <div class='d-flex flex-column text-center'>
+                                
+                                <h1>".$producto->getPronombre()."</h1>
+                                
+                                <div class='bg-steam-lightgreen bdr-steam-nofocus p-1'>
+                                    " . $header . "
+                                </div>
                             </div>
                         </div>
 
-                        <div class='text-center'>
+                        <div class='text-center bg-steam-darkgreen bdr-steam-nofocus'>
                             <!--- TEXTO DESCRIPTIVO DEL PRODUCTO --->
                             <p> ".$producto->getProdetalle()." </p>
                         </div>
                         
                     </div>
                     
-                    <div class='bg-steam-darkgreen bdr-steam-focus d-flex overflow-x-scroll h-25 w-75 m-auto'>
+                    <div class='bg-steam-darkgreen bdr-steam-focus d-flex overflow-x-scroll w-75 m-auto' style='height: 200px'>
                         ".$previewDiv."
                     </div>
                     
@@ -58,7 +63,7 @@ if (!empty($data)) {
                 <div class='col-6 bg-steam-lightgreen bdr-steam-nofocus d-flex overflow-x-scroll'>
                     <!-- TRAILER/VIDEO/IMAGEN AL RESPECTO SI FUERA EL CASO -->
                     <div class='m-auto'>
-                        <iframe width='800' height='600' class='bg-steam-lightgreen bdr-steam-nofocus' src='https://www.youtube.com/embed/edYCtaNueQY?si=WbFaQlTLEAdBxTlt' frameborder='0' allowfullscreen></iframe>
+                        <iframe width='800' height='600' class='bg-steam-lightgreen bdr-steam-nofocus' src='https://www.youtube.com/embed/".$producto->getIdvideoyt()."' frameborder='0' allowfullscreen></iframe>                                 
                     </div>
                 </div>                
         </div>
