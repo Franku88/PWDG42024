42214c2113ad25539c9b9a0c68f8025e5c74b03b
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-11-24 03:59:12 -0300

    Generacion de menu dinámicamente (usando array de menues)

--- TPF/View/Structure/HeadSafe.php
+++ TPF/View/Structure/HeadSafe.php
@@ -1,73 +1,89 @@
 <?php
     //Verificacion de sesion valida (para mostrar pagina segura)
-    $session = new Session();
-    $sessionValida = $session->validar();
-    $menues = []; //Array con menues permitidos para dicho rol
+    $sesion = new Session();
+    $sesionValida = $sesion->validar();
+    $menues = []; //Array con menues permitidos para rol actual
 
-    if (!$sessionValida) {
-        header("Location: ".BASE_URL."/View/Pages/SesionInvalida/SesionInvalida.php");
-        exit();
-    }
-
-    if ($sessionValida) {
-        $objRol = $session->getRol()[0];
-        $usuarioRolId = $objRol->getIdrol();
-        
+    if ($sesionValida) {
+        $objRol = $sesion->getRoles()[0];
         $menuRoles = (new ABMMenuRol())->buscar(['rol'=> $objRol]); // [objMenuRol($objmenu, $objrol),objMenuRol($objmenu2, $objrol),...]
+
         foreach($menuRoles as $menuRol) {
             array_push($menues, ($menuRol->getObjMenu()));
         }
-
         foreach($menues as $menu) { //Busca menues hijos y los agrega al array de menues
             $hijos = (new ABMMenu())->buscar(['padre' => $menu]);
             $menues = array_merge($menues, $hijos);
         }
-    } else {
-        $usuarioRolId = 0;
-    }
 
-    if ($usuarioRolId == 3) { //Si es cliente (idrol = 3)
-        $usuario = $session->getUsuario();
-
-        $compras = (new ABMCompra())->buscar(['usuario'=> $usuario]);
-        $compraEstados = (new ABMCompraEstado)->buscar(['idcompraestadotipo' => 1, 'cefechafin' => null]); //Toda compraEstado de la bd
-        
-        $i = 0;
-        $j = 0;
-        $encontrado = false;
-        $compraEstado = null;
-        while (!$encontrado && ($i < count($compraEstados))) {
-            while (!$encontrado && ($j < count($compras))) {
-                $encontrado = ($compraEstados[$i]->getObjCompra()->getIdcompra()) == ($compras[$j]->getIdcompra());
-                if ($encontrado) {
-                    $compraEstado = $compraEstados[$i];
-                }
-                $j++;
-            }
+        // Crea compra en estadotipo 1 (carrito) si no lo tiene, (SOLO PARA CLIENTES)
+        if ($sesion->esCliente()) { //Si es cliente (idrol = 3)
+            $usuario = $sesion->getUsuario();
+    
+            $compras = (new ABMCompra())->buscar(['usuario'=> $usuario]);
+            $compraEstados = (new ABMCompraEstado)->buscar(['idcompraestadotipo' => 1, 'cefechafin' => null]); //Toda compraEstado de la bd
+            
+            $i = 0;
             $j = 0;
-            $i++;
-        }
-        
-        if ($compraEstado == null) {
-            $param1['cofecha'] = (new DateTime('now', (new DateTimeZone('-03:00'))))->format('Y-m-d H:i:s');
-            $param1['usuario'] = $session->getUsuario();
-            $altaCompra = (new ABMCompra())->alta($param1);
-
-            if ($altaCompra) {
-                $compras = (new ABMCompra)->buscar(['usuario'=> $param1['usuario'], 'cofecha' => $param1['cofecha']]);
-                $compraEstadoTipos = (new ABMCompraEstadoTipo())->buscar(['idcompraestadotipo'=> 1]);
-
-                $altaCompraEstado = (new ABMCompraEstado())->alta([
-                    'objCompra'=> $compras[0], 
-                    'objCompraEstadoTipo'=> $compraEstadoTipos[0], 
-                    'cefechaini' => $compras[0]->getCofecha()]
-                );
-
-                $compraEstado = (new ABMCompraEstado)->buscar(['objCompra'=> $compras[0], 'idcompraestadotipo' => 1, 'cefechafin' => null]); //Toda compraEstado de la bd
+            $encontrado = false;
+            $compraEstado = null;
+            while (!$encontrado && ($i < count($compraEstados))) {
+                while (!$encontrado && ($j < count($compras))) {
+                    $encontrado = ($compraEstados[$i]->getObjCompra()->getIdcompra()) == ($compras[$j]->getIdcompra());
+                    if ($encontrado) {
+                        $compraEstado = $compraEstados[$i];
+                    }
+                    $j++;
+                }
+                $j = 0;
+                $i++;
+            }
+            
+            if ($compraEstado == null) {
+                $param1['cofecha'] = (new DateTime('now', (new DateTimeZone('-03:00'))))->format('Y-m-d H:i:s');
+                $param1['usuario'] = $sesion->getUsuario();
+                $altaCompra = (new ABMCompra())->alta($param1);
+    
+                if ($altaCompra) {
+                    $compras = (new ABMCompra)->buscar(['usuario'=> $param1['usuario'], 'cofecha' => $param1['cofecha']]);
+                    $compraEstadoTipos = (new ABMCompraEstadoTipo())->buscar(['idcompraestadotipo'=> 1]);
+    
+                    $altaCompraEstado = (new ABMCompraEstado())->alta([
+                        'objCompra'=> $compras[0],
+                        'objCompraEstadoTipo'=> $compraEstadoTipos[0],
+                        'cefechaini' => $compras[0]->getCofecha()]
+                    );
+    
+                    $compraEstado = (new ABMCompraEstado)->buscar(['objCompra'=> $compras[0], 'idcompraestadotipo' => 1, 'cefechafin' => null]); //Toda compraEstado de la bd
+                }
             }
         }
-        
+    } else { //Redirige a pagina no segura (UNICA DIFERENCIA CON Head.php)
+        header("Location: ".BASE_URL."/View/Pages/SesionInvalida/SesionInvalida.php");
+        exit();
     }
+
+    // Opciones a mostrar en header (dinamicamente)
+    $menuHtml = "<div class='mx-3'>
+        <a class='btn btn-primary btn-steam my-1' href='".BASE_URL."/View/Pages/Catalogo/Catalogo.php'> Catalogo </a>";
+    
+    foreach ($menues as $cadaMenu) {
+        if ($cadaMenu->getPadre() != null) {
+            $menuHtml .= "<a class='btn btn-primary btn-steam my-1' href='".BASE_URL.$cadaMenu->getMeurl()."'> ".$cadaMenu->getMenombre()." </a>";
+        }
+    }
+
+    $menuHtml .= "</div>
+        <div class='mx-3'>";
+
+    if ($sesionValida) { 
+        $menuHtml .=  "<a class='btn btn-primary btn-steam my-1' href='".BASE_URL."/View/Pages/Perfil/Perfil.php'>Perfil</a>
+        <a class='btn btn-primary btn-steam my-1' href='".BASE_URL."/View/Pages/Logout/Logout.php'>Logout</a>";
+    } else { 
+        $menuHtml .= "<a class='btn btn-primary btn-steam my-1' href='".BASE_URL."/View/Pages/Login/Login.php'>Login</a>
+        <a class='btn btn-primary btn-steam my-1' href='".BASE_URL."/View/Pages/Register/Register.php'>Register</a>";
+    }
+    $menuHtml .= "</div>";
 ?>
 
 <!DOCTYPE html>
@@ -106,45 +122,21 @@
 
 </head>
 
-<body class="d-flex flex-column min-vh-100 bg-steam-darkgreen"> <!-- Comienza body !-->
+<body class="d-flex flex-column min-vh-100 bg-steam-darkgreen"> <!-- Comienza body -->
     <header>
         <!-- Barra de navegación -->
         <nav class="navbar navbar-expand-sm bg-steam-lightgreen bdr-steam-nofocus justify-content-between">
-            <div class="d-flex text-center mx-1 ">
-                <img src="<?php echo(BASE_URL);?>/View/Media/Site/logo.png" height="50" width="50"> </img>
             
-                <div class="mx-2 my-1">
-                    <a class="btn btn-primary btn-steam3 mx-2" href="<?php echo BASE_URL; ?>/index.php">Inicio</a> <!-- Index button -->
-                </div>
+            <div class="d-flex mx-1">
+                <a class=" text-decoration-none" href="<?php echo BASE_URL; ?>/index.php"> <!-- Index button -->
+                    <div class='d-flex'>
+                        <img src="<?php echo(BASE_URL);?>/View/Media/Site/logo.png" height="50" width="50"> </img>
+                        <h5 class='text-black my-auto mx-2 b'> <strong> Black Mesa&reg;</strong></h6>
+                    </div>
+                </a>
             </div>
             
-            <div class="mx-3">
-                <a class="btn btn-primary btn-steam my-1" href="<?php echo BASE_URL; ?>/View/Ejercicios/Catalogo/Catalogo.php"> Catalogo </a>
-                
-                <?php if ($usuarioRolId == 1) { ?>
-                    <a class="btn btn-primary btn-steam my-1" href="<?php echo BASE_URL; ?>/View/Ejercicios/AdministrarUsuarios/AdministrarUsuarios.php">Administrar Usuarios</a>
-                <?php } ?>
-
-                <?php if ($usuarioRolId == 2) { ?>
-                    <a class="btn btn-primary btn-steam my-1" href="<?php echo BASE_URL; ?>/View/Ejercicios/AdministrarProductos/AdministrarProductos.php">Administrar Productos</a>
-                    <a class="btn btn-primary btn-steam my-1" href="<?php echo BASE_URL; ?>/View/Ejercicios/AdministrarCompras/AdministrarCompras.php">Administrar Compras</a>
-                <?php } ?>
-
-                <?php if ($usuarioRolId == 3) { ?>
-                    <a class="btn btn-primary btn-steam my-1" href="<?php echo BASE_URL; ?>/View/Ejercicios/Carrito/Carrito.php">Carrito</a>
-                    <a class="btn btn-primary btn-steam my-1" href="<?php echo BASE_URL; ?>/View/Ejercicios/MisCompras/MisCompras.php">Mis Compras</a>
-                <?php } ?>
-            </div>
-
-            <div class="mx-3">
-                <?php if ($sessionValida) { ?>
-                    <a class="btn btn-primary btn-steam my-1" href="<?php echo BASE_URL; ?>/View/Ejercicios/Perfil/Perfil.php">Perfil</a>
-                    <a class="btn btn-primary btn-steam my-1" href="<?php echo BASE_URL; ?>/View/Ejercicios/Logout/Logout.php">Logout</a>
-                <?php } else { ?>
-                    <a class="btn btn-primary btn-steam my-1" href="<?php echo BASE_URL; ?>/View/Ejercicios/Login/Login.php">Login</a>
-                    <a class="btn btn-primary btn-steam my-1" href="<?php echo BASE_URL; ?>/View/Ejercicios/Register/Register.php">Register</a>
-                <?php } ?>
-            </div>
+            <?php echo $menuHtml?>
             
         </nav>
-    </header>  
+    </header>
