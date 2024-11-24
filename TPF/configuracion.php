87d96789f1e73279aa411e4a35e0b628dedffb5e
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-11-24 01:07:25 -0300

    fix en comments

--- TPF/configuracion.php
+++ TPF/configuracion.php
@@ -7,14 +7,13 @@ header('Cache-Control: no-cache, must-revalidate');
 
 // Paths constants
 $dir = '/PWDG42024/TPF'; // Directorio del proyecto dentro del servidor (default: dentro de "C:/xampp/htdocs")
-define('BASE_URL', 'http://'.$_SERVER['HTTP_HOST'].$dir ); // Para hrefs (links)
-define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'].$dir); // Define raiz del proyecto como cons. ROOT_PATH
-define('STRUCTURE_PATH', ROOT_PATH.'/View/Structure'); //Define path de la estructura (Header, footer, menu, etc)
-define('INCLUDE_PATH', STRUCTURE_PATH.'/Include'); // Define path de scripts a utilizar
-$_SERVER["ROOT"] = ROOT_PATH; //$SESSION: Array (inicalmente vacio) que persiste durante sesion del usuario
+define('BASE_URL', 'http://'.$_SERVER['HTTP_HOST'].$dir ); // URL principal del sitio (direccion web)
+define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'].$dir); // Raiz del proyecto (directorio local)
+define('STRUCTURE_PATH', ROOT_PATH.'/View/Structure'); // Directorio de la estructura (Header, footer, menu, etc)
+//define('INCLUDE_PATH', STRUCTURE_PATH.'/Include'); // Directorio de la estructura de scripts a utilizar
+$_SERVER["ROOT"] = ROOT_PATH; //Establece directorio raiz en array
 
 // Global includes
-include_once(ROOT_PATH.'/Util/autoloader.php'); //Funciones usadas en toda la sesion
-// require_once 'vendor/autoload.php';
-
+include_once(ROOT_PATH.'/Util/autoloader.php'); // Funciones usadas en toda la sesion
+// require_once 'vendor/autoload.php'; // Con Composer
 ?>
