0f3b179c9cac44387c505e6d4cef9128135206bd
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-11-24 03:54:33 -0300

    Se redefinieron constantes (agregando CURRENT_URL)

--- TPF/configuracion.php
+++ TPF/configuracion.php
@@ -7,7 +7,9 @@ header('Cache-Control: no-cache, must-revalidate');
 
 // Paths constants
 $dir = '/PWDG42024/TPF'; // Directorio del proyecto dentro del servidor (default: dentro de "C:/xampp/htdocs")
-define('BASE_URL', 'http://'.$_SERVER['HTTP_HOST'].$dir ); // URL principal del sitio (direccion web)
+define('DOMAIN_URL', 'http://'.$_SERVER['HTTP_HOST']); // URL del domino
+define('CURRENT_URL', DOMAIN_URL.$_SERVER['REQUEST_URI']); //URL Actual
+define('BASE_URL', DOMAIN_URL.$dir ); // URL principal del sitio (direccion web)
 define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'].$dir); // Raiz del proyecto (directorio local)
 define('STRUCTURE_PATH', ROOT_PATH.'/View/Structure'); // Directorio de la estructura (Header, footer, menu, etc)
 //define('INCLUDE_PATH', STRUCTURE_PATH.'/Include'); // Directorio de la estructura de scripts a utilizar
