b53a7ed089a8f3a2ddd0a1df6a1f1ec711a7c6d1
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-11-25 22:19:22 -0300

    perfil valida individualmente

--- TPF/View/Pages/Perfil/Perfil.php
+++ TPF/View/Pages/Perfil/Perfil.php
@@ -1,6 +1,10 @@
 <?php
 include_once '../../../configuracion.php';
-include STRUCTURE_PATH . '/HeadSafe.php';
+include STRUCTURE_PATH . '/Head.php';
+if(!$sesion->validar()) {
+    header("Location: ".BASE_URL."/View/Pages/SesionInvalida/SesionInvalida.php");
+    exit();
+}
 ?>
 
 <div class="d-flex justify-content-center align-items-start gap-3">
