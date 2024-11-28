9911ad7c21b73a552f5e1c259379a6ba2e65c0aa
Franku88 <fabian.benitez@est.fi.uncoma.edu.ar>
2024-11-27 23:03:57 -0300

    Action limpio

--- TPF/View/Pages/Catalogo/Action/ObtenerProducto.php
+++ TPF/View/Pages/Catalogo/Action/ObtenerProducto.php
@@ -1,23 +1,8 @@
 <?php
 include_once '../../../../configuracion.php';
+$data = Funciones::data_submitted();
 
-$data = Funciones::data_submitted(); 
-$salida = [];
-if (!empty($data)) {
-    $productos = (new ABMProducto())->buscar($data);
-    foreach($productos as $producto) {
-        if ($producto->getProdeshabilitado() == null) {
-            $nuevoElem['icon'] = "../../Media/Product/".$producto->getIdproducto()."/icon.png";
-            $nuevoElem['idproducto'] = $producto->getIdproducto();
-            $nuevoElem['pronombre'] = $producto->getPronombre();
-            $nuevoElem['prodetalle'] = $producto->getProdetalle();
-            $nuevoElem['procantstock'] = $producto->getProcantstock();
-            $nuevoElem['proprecio'] = $producto->getProprecio();
-            $nuevoElem['prodeshabilitado'] = $producto->getProdeshabilitado();
-            $nuevoElem['idvideoyt'] = $producto->getIdvideoyt();
-            array_push($salida, $nuevoElem);
-        }
-    }
-} 
-echo json_encode($salida);
+$respuesta = (new ABMProducto())->listarProductos($data); //Obtiene array unitario con producto idproducto en $data
+
+echo json_encode($respuesta);
 ?>
