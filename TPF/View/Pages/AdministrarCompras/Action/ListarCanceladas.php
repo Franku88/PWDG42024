32a6714bcb0d031767ec5c2f317a8c903c7893f3
jovirecast <jreyescastello@gmail.com>
2024-11-27 23:46:50 -0300

    actions administrarCompras

--- TPF/View/Pages/AdministrarCompras/Action/ListarCanceladas.php
+++ TPF/View/Pages/AdministrarCompras/Action/ListarCanceladas.php
@@ -1,20 +1,8 @@
 <?php
 include_once '../../../../configuracion.php';
 
-$data = Funciones::data_submitted(); 
-$salida = [];
-if (!empty($data)) {
-    $objCompraEstadoTipo = (new ABMCompraEstadoTipo())->buscar(['idcompraestadotipo' => 4])[0];
-    $compras = (new ABMCompraEstado())->buscar([ 'objCompraEstadoTipo' => $objCompraEstadoTipo , 'cefechafin' => null]);
-    foreach($compras as $compra) {
-        $nuevoElem['idcompra'] = $compra->getObjCompra()->getIdcompra();
-        $nuevoElem['estado'] = $compra->getObjCompraEstadoTipo()->getCetdescripcion();
-        $nuevoElem['fechaInicio'] = $compra->getCefechaini();
-        $nuevoElem['fechaFin'] = $compra->getCefechafin();
-        $nuevoElem['usuario'] = $compra->getObjCompra()->getObjUsuario()->getUsnombre();
-        array_push($salida, $nuevoElem);
-    }
-} 
+$data = Funciones::data_submitted();
+
+$salida = (new ABMCompraEstadoTipo())->listarCanceladas();
 
 echo json_encode($salida);
-?>
--- TPF/View/Pages/AdministrarCompras/Action/ListarEntrantes.php
+++ TPF/View/Pages/AdministrarCompras/Action/ListarEntrantes.php
@@ -1,21 +1,9 @@
 <?php
 include_once '../../../../configuracion.php';
 
-$data = Funciones::data_submitted(); 
-$salida = [];
-if (!empty($data)) {
-    $objCompraEstadoTipo = (new ABMCompraEstadoTipo())->buscar(['idcompraestadotipo' => 2])[0];
-    $compras = (new ABMCompraEstado())->buscar([ 'objCompraEstadoTipo' => $objCompraEstadoTipo , 'cefechafin' => 'null']);
-    foreach($compras as $compra) {
-        $nuevoElem['idcompra'] = $compra->getObjCompra()->getIdcompra();
-        $nuevoElem['estado'] = $compra->getObjCompraEstadoTipo()->getCetdescripcion();
-        $nuevoElem['fechaInicio'] = $compra->getCefechaini();
-        $nuevoElem['fechaFin'] = $compra->getCefechafin();
-        $nuevoElem['usuario'] = $compra->getObjCompra()->getObjUsuario()->getUsnombre();
-        $nuevoElem['idcompraEstado'] = $compra->getIdcompraestado();
-        array_push($salida, $nuevoElem);
-    }
-} 
+$data = Funciones::data_submitted();
+
+$salida = (new ABMCompraEstadoTipo())->listarEntrantes();
 
 echo json_encode($salida);
 ?>
